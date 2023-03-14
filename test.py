936c38e7068588e33da3b4cf8317f0854774df47
Ahmad Jawabreh <ahmadjawabreh20@gmail.com>
2023-03-14 15:50:18 +0200

    Update threshold optimization
    Closes #2

--- test.py
+++ test.py
@@ -1,96 +1,107 @@
-# Face Identification Algorithm using MTCNN for face detection, FaceNet for face feature extraction, and SVM for Classification 
-from sklearn.svm import SVC
-from numpy import load
-from numpy import expand_dims
-from numpy import asarray
-from keras.models import load_model
-from PIL import Image
-from mtcnn.mtcnn import MTCNN
+# Masked Face Identification Algorithm using MTCNN for face detection, FaceNet for face feature extraction, and SVM for Classification 
+import cv2
 import numpy as np
+from mtcnn import MTCNN
+from tensorflow.keras.models import load_model
+from sklearn.preprocessing import Normalizer, LabelEncoder
+from sklearn.svm import SVC
+import matplotlib.pyplot as plt
 
-# extract a single face from a given photograph using MTCNN
-def extract_face(filename, required_size=(160, 160)):
-    # load image from file
-    image = Image.open(filename)
-    # convert to RGB, if needed
-    image = image.convert('RGB')
-    # convert to array
-    pixels = asarray(image)
-    # create the detector, using default weights
-    detector = MTCNN()
-    # detect faces in the image
-    results = detector.detect_faces(pixels)
-    # check if face was detected
-    if results:
-        # extract the bounding box from the first face
-        x1, y1, width, height = results[0]['box']
-        # bug fix
-        x1, y1 = abs(x1), abs(y1)
-        x2, y2 = x1 + width, y1 + height
-        # extract the face
-        face = pixels[y1:y2, x1:x2]
-        # resize pixels to the model size
-        image = Image.fromarray(face)
-        image = image.resize(required_size)
-        face_array = asarray(image)
-        return face_array
-    else:
-        return None
+# Load face detection model
+detector = MTCNN()
 
-# get the face embedding for one face using FaceNet
-def get_embedding(model, face_pixels):
-    # scale pixel values
-    face_pixels = face_pixels.astype('float32')
-    # standardize pixel values across channels (global)
-    mean, std = face_pixels.mean(), face_pixels.std()
-    face_pixels = (face_pixels - mean) / std
-    # transform face into one sample
-    samples = expand_dims(face_pixels, axis=0)
-    # make prediction to get embedding
-    yhat = model.predict(samples)
-    return yhat[0]
+# Load face recognition model
+facenet_model = load_model('/home/jawabreh/Desktop/HumaneX_Project/Face-Recognition/facenet_keras.h5')
 
-# load the face embeddings dataset
-data = load('./Embeddings_Dataset/Embeddings.npz')
+# Load face embeddings
+data = np.load('/home/jawabreh/Desktop/HumaneX_Project/Face-Recognition/Embeddings_Dataset/Embeddings.npz')
 trainX, trainy = data['arr_0'], data['arr_1']
-print('Loaded train embeddings: ', trainX.shape, trainy.shape)
 
-# load the facenet model
-model = load_model('facenet_keras.h5')
-print('Loaded FaceNet model')
+# Normalize input vectors
+in_encoder = Normalizer(norm='l2')
+trainX = in_encoder.transform(trainX)
 
-# extract the face embedding for a new image
-image = extract_face('./Data/Team_Validation_Dataset/Ahmad/2.jpeg')
-embedding = get_embedding(model, image)
-testX = asarray([embedding])
-print('Loaded test embedding: ', testX.shape)
-# 7.3 12 ravilya unknown
-# fit SVM model
+# Label encode targets
+out_encoder = LabelEncoder()
+out_encoder.fit(trainy)
+trainy = out_encoder.transform(trainy)
+
+# Define the classes
+class_names = out_encoder.classes_
+class_names = np.append(class_names, 'unknown')
+
+# Train SVM classifier
 model = SVC(kernel='linear', probability=True)
 model.fit(trainX, trainy)
 
-# compute distances between embeddings of test image and all training images
-distances = np.linalg.norm(trainX - testX, axis=1)
+# Define function to extract face embeddings
+def extract_face_embeddings(image):
+    # Detect faces in the image
+    faces = detector.detect_faces(image)
+    if not faces:
+        return None
+    # Extract the first face only
+    x1, y1, width, height = faces[0]['box']
+    x2, y2 = x1 + width, y1 + height
+    face = image[y1:y2, x1:x2]
+    # Resize face to the size required by facenet model
+    face = cv2.resize(face, (160, 160))
+    # Preprocess the face for facenet model
+    face = face.astype('float32')
+    mean, std = face.mean(), face.std()
+    face = (face - mean) / std
+    face = np.expand_dims(face, axis=0)
+    # Generate embeddings using facenet model
+    embeddings = facenet_model.predict(face)
+    return embeddings[0]
 
-# set threshold for the minimum distance allowed
-threshold = 0
+# Define function to identify the identity of an input image
+def identify_person(image):
+    # Extract face embeddings from input image
+    embeddings = extract_face_embeddings(image)
+    if embeddings is None:
+        return None, None
+    # Normalize embeddings
+    embeddings = in_encoder.transform([embeddings])
+    # Predict the identity and confidence using SVM classifier
+    prediction = model.predict(embeddings)
+    confidence = model.predict_proba(embeddings)[0][prediction] * 100
+    prediction = out_encoder.inverse_transform(prediction)
+    return prediction[0].item(), confidence
 
-# defining the authorized users
-authorized_users = ['Ravilya', 'Ahmad', 'Milena']
-
-# predict the class of the new image
-if np.min(distances) <= threshold:
-    # get the predicted class and its corresponding probability
-    yhat_test = model.predict(testX)[0]
-    proba = model.predict_proba(testX)[0]
-    
-    if yhat_test in authorized_users:
-        print('Prediction Result: ', yhat_test, ' - Authorized')
-        print('Confidence Rate:', proba[authorized_users.index(yhat_test)])
+# Define function to identify the identity and confidence of an input image
+def identify_person_with_unknown(image, threshold=0.9):
+    # Extract face embeddings from input image
+    embeddings = extract_face_embeddings(image)
+    if embeddings is None:
+        return None, None
+    # Normalize embeddings
+    embeddings = in_encoder.transform([embeddings])
+    # Predict the identity and confidence using SVM classifier
+    predictions = model.predict_proba(embeddings)[0]
+    max_idx = np.argmax(predictions)
+    if predictions[max_idx] >= threshold:
+        prediction = out_encoder.inverse_transform([max_idx])
+        confidence = predictions[max_idx] * 100
+        return prediction[0].item(), confidence
     else:
-         print('Unauthorized')
-    
-   
+        return "unknown", None
+
+# Example usage
+image = cv2.imread('/home/jawabreh/Desktop/testSis/ukown4.jpg')
+person, confidence = identify_person_with_unknown(image)
+if person is None:
+    print('No face detected in the input image!')
+elif person == "unknown":
+    text = "Unknown person"
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.title(text)
+    plt.axis('off')
+    plt.show()
 else:
-    # consider the test image an unknown face
-    print('Unknown Face')
+    # Display the predicted name and confidence probability
+    text = f'Predicted: {str(person)} ({confidence:.2f}%)'
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.title(text)
+    plt.axis('off')
+    plt.show()
